584c482ddacc67da7653c1075fe635785f8e0076
Luv8436 <luvkumar8436@gmail.com>
2024-10-19 12:59:27 +0530

    changed the intro summary and image layout

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -29,15 +29,19 @@ const Introduction = () => {
           />
         </div>
 
-        <div className="intro-summary">
-          <p>
-          <img className="radharani-image" src={require("./images/Beautiful-wallpaper-radha.jpg")}  alt="radha rani" />
-          Mathura and Vrindavan are the two sacred towns in the heart of Uttar Pradesh, are timeless destinations that attracts tourist around the world.
-  
-          Mathura, the birthplace of Lord Krishna, has its spiritual history and vibrant culture which tourist wants to explore. 
-      
-          From the ancient temples and ghats along the Yamuna River to the lively celebrations of Janamasthami and Holi, every street of Mathura has its own story related to Lord Krishna.  
-          </p>
+        <div className="summary-container">
+          <div className="intro-summary">
+          
+            Mathura and Vrindavan are the two sacred towns in the heart of Uttar Pradesh, are timeless destinations that attracts tourist around the world.
+    
+            Mathura, the birthplace of Lord Krishna, has its spiritual history and vibrant culture which tourist wants to explore. 
+        
+            From the ancient temples and ghats along the Yamuna River to the lively celebrations of Janamasthami and Holi, every street of Mathura has its own story related to Lord Krishna.  
+          </div>
+          <div className="radharani-image">
+          <img  src={require("./images/Beautiful-wallpaper-radha.jpg")}  alt="radha rani" />
+          </div >
+          
         </div>
         
         
