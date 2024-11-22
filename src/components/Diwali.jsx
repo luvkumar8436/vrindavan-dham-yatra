a4a4b34da8b826d6bcc6ab54e5950427083b4c25
Luv8436 <luvkumar8436@gmail.com>
2024-11-23 02:58:38 +0530

    added new year background

--- src/components/Diwali.jsx
+++ src/components/Diwali.jsx
@@ -1,22 +1,19 @@
 import './Diwali.css'
 import React from "react"; 
-import ContainedButtons from "./ContainedButtons";
 
 const Diwali = () => {
     return (
         <div className='diwali-main-container'>
-            <div className="diwali-container">
-                <div className='diwali-inside-container' >
-                    <div className='diwali-heading'>
-                    Celebrate this Diwali in Mathura Vrindavan.
-                    </div>
-                    <div className='diwali-para'>
-                        Book packages from our website and we will make sure that you will get the best tour experience of your life with us and make this journey memorable for you.
-                        <ContainedButtons class="offer-btn"  text="Avail offer" />
-                    </div>
+            <div className='diwali-container' >
+                <div className='diwali-heading'>
+                    Celebrate this New Year in Mathura Vrindavan
                 </div>
-                
+                <button>
+                    Click Here
+                </button>
+                <p>Heavy Discounts</p>
             </div>
+            
         </div>
         
     )
