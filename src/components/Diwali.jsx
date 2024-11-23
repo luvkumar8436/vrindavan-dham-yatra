597ce52551573429650f653667d88163b357a5e7
Luv8436 <luvkumar8436@gmail.com>
2024-11-23 16:21:36 +0530

    changing the destination container to grid

--- src/components/Diwali.jsx
+++ src/components/Diwali.jsx
@@ -3,19 +3,23 @@ import React from "react";
 
 const Diwali = () => {
     return (
-        <div className='diwali-main-container'>
-            <div className='diwali-container' >
-                <div className='diwali-heading'>
-                    Celebrate this New Year in Mathura Vrindavan
-                </div>
-                <button>
-                    Click Here
-                </button>
-                <p>Heavy Discounts</p>
+        <div className='container-outside'>
+            <div className='festive-offer-container' >
+                    <div className='festive-heading'>
+                    Celebrate the New Year in the Divine Bliss of Mathura & Vrindavan!
+                    </div>
+                    <p>Start your year on a spiritual and joyful note with our exclusive New Year Tourism Offers on Mathura, Vrindavan, and nearby sacred destinations. Immerse yourself in the serene beauty of Krishna's birthplace, explore the vibrant ghats of Mathura, witness the enchanting aarti at Vrindavan's temples, and discover the rustic charm of Govardhan and Barsana. Enjoy specially curated packages that include guided tours, comfortable accommodations, festive events, and delectable local cuisine.</p>
+                    <p> Book now to avail early-bird discounts and complimentary cultural experiences. Let the divine aura of these holy places make your New Year truly unforgettable!</p>
+                    <button>
+                        Click Here
+                    </button>
+                    <div className='discount' >Heavy Discounts</div>
+                    
             </div>
-            
+            <div className='festive-img-container'>
+                <img src="https://static.vecteezy.com/system/resources/previews/052/106/592/non_2x/happy-new-year-2025-illustration-with-golden-typography-lettering-and-christmas-ball-on-falling-confetti-background-winter-holiday-design-for-flyer-banner-greeting-card-celebration-poster-party-vector.jpg" alt="new year 2025" />
+            </div>    
         </div>
-        
     )
 }
 
