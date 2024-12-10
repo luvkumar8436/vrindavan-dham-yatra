10eae5d11cc36c25a5b103edcc32d0023253d4a4
Luv8436 <luvkumar8436@gmail.com>
2024-12-11 00:00:45 +0530

    added module css for package component

--- src/components/Package.jsx
+++ src/components/Package.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import "./Package.css"
+import styles from "./Package.module.css"
 import ContainedButtons from "./ContainedButtons";
 import { Link } from "react-router-dom";
 
 const Package = (props) => {
     return (
-        <div className="package">
+        <div className={styles["package"]}>
             <img src={props.image} alt="" />
-            <div className="package-container-info" >
-                <div className="tour-subheading-container" >
-                    <div className="duration" >{props.duration}</div>
-                    <div className="rating">&#9733; {props.rating}</div>
-                    <div className="tour-headline">{props.tourHeadline}</div>
+            <div className={styles["package-container-info"]} >
+                <div className={styles["tour-subheading-container"]} >
+                    <div className={styles["duration"]} >{props.duration}</div>
+                    <div className={styles["rating"]}>&#9733; {props.rating}</div>
+                    <div className={styles["tour-headline"]}>{props.tourHeadline}</div>
                 </div>
-                <div className="tour-subinfo-container">
-                    <div className="tour-places" >{props.tourPlaces}</div>
+                <div className={styles["tour-subinfo-container"]}>
+                    <div className={styles["tour-places"]} >{props.tourPlaces}</div>
                     <div ><Link  ><ContainedButtons class="enquiry"  text="Enquire Now" /></Link></div>
                     <div ><Link to={"/tourist-packages/"+props.tourHeadline.split(" ").join("-") } state={{ duration: props.duration , places: props.tourPlaces , tourData: props.tourData}} > <ContainedButtons class="info" text="Know More" /></Link></div>
                 </div>  
